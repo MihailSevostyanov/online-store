41c077fee44b8547105c5704a3760adc15ffa0da
Mihail Sevostyanov <sevamihael141095@gmail.com>
2024-03-17 11:35:49 +0200

    add conftest.py

--- tests/test_category.py
+++ tests/test_category.py
@@ -2,12 +2,7 @@ import pytest
 from src.category import Category
 
 
-@pytest.fixture
-def test_correct_init_category():
-    return Category("Смартфоны", "Cмартфоны, как средство не только коммуникации, "
-                                 "но и получения дополнительных функций для удобства жизни", [1, 2, 3, 4])
-
-
+@pytest.mark.usefixtures("test_correct_init_category")
 def test_init(test_correct_init_category):
     assert test_correct_init_category.name == 'Смартфоны'
     assert test_correct_init_category.description == ('Cмартфоны, как средство не только коммуникации, '
@@ -15,12 +10,6 @@ def test_init(test_correct_init_category):
     assert test_correct_init_category.products == [1, 2, 3, 4]
 
 
-@pytest.fixture
-def test_correct_init_numb():
-    return Category("Смартфоны", "Cмартфоны, как средство не только коммуникации, "
-                                 "но и получения дополнительных функций для удобства жизни", [1, 2, 3, 4])
-
-
-def test_summ_numb(test_correct_init_numb):
-    assert test_correct_init_numb.total_number_of_unique_products == 4
-    assert test_correct_init_numb.total_number_of_categories == 1
+def test_summ_numb(test_correct_init_category):
+    assert test_correct_init_category.total_number_of_unique_products == 4
+    assert test_correct_init_category.total_number_of_categories == 1
